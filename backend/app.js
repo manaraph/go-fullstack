6d74c64ebefcba65882a08621b753d5cc4709057
Manasseh Omachonu <manaraph225@gmail.com>
2019-10-13 09:27:49 +0100

    Send back data through an api route

--- backend/app.js
+++ backend/app.js
@@ -2,25 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Request received');
-  next();
+app.use('/api/stuff', (req, res) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'My first thing',
+      description: 'All of the info about my first thing',
+      imageUrl: '',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'My second thing',
+      description: 'All of the info about my second thing',
+      imageUrl: '',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+
+  res.status(200).json(stuff);
 });
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-})
-
-app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful!' }); 
-  next();
-});
-
-app.use((req, res) => {
-  console.log('Response sent successfully');
-  
-})
 
 
 module.exports = app;
