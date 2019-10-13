1a6ede42d8f105093aa4ceed7ddf940ac245c512
Manasseh Omachonu <manaraph225@gmail.com>
2019-10-13 09:54:56 +0100

    Creating post requests

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+
+app.post('/api/stuff', (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Thing created successfully!'
+  });
 });
 
 app.use('/api/stuff', (req, res) => {
@@ -31,6 +41,7 @@ app.use('/api/stuff', (req, res) => {
 
   res.status(200).json(stuff);
 });
+
 
 
 module.exports = app;
